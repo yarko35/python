dbb4a4c846ec248b663c681e768767a83c364f1e
yarko35 <yarkinokular@gmail.com>
2021-02-25 09:10:03 +0300

    classlar baslandı

---
+++ OOP/class.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 25 08:10:14 2021
+
+@author: yarki
+"""
+
+#%% Classes
+
+
+kole="Ege"
+koleninYası=22
+koleninadresi="Her zaman benim yanım kardeş"
+
+class kole(object):
+    #attribute=kolemizin özellikleri,yaş,adres,isim gibi
+    #behaviour=davranışları
+    pass#ileride kole sınıfımı doldurucam diye programa söylemiş oluyorum 
+    
+#Class içerisi dışarısı kavramlarını unutma
+#aksi takdirde IndentationError hatasını alabilirsin.(fazla boşluk bırakma)
+koleyaratma= kole()#kölemizi yaratmış oluyoruz böylelikle
