7313502326011fd7d594817630b17e6949d219f9
yarko35 <yarkinokular@gmail.com>
2021-03-03 10:18:02 +0300

    yarısında birakildi

---
+++ Python AI/OOP/encapsulation.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar  3 06:38:41 2021
+
+@author: yarki
+"""
+#tanım olarak variable ve methodlara erişimi kısıtlamak demektir.
+#---------------------kodda bir hata var bankAccount()takes no arguments
+class bankAccount(object):
+    def __init(self,money,name,adress):
+        #buradaki variable lar global olduğu için erişilebiliyor
+        #iki tane alt çizgi koyduğumuz an private olmuş oluyor
+        self.__money=money
+        self.name=name 
+        self.adress=adress
+
+
+personEge=bankAccount(5000,"Ege Bağçıvan","Narlıdere")
+personYarko=bankAccount(4820,"Yavuz Yarkın Okular","Balçova")
+personErdem=bankAccount(2324,"Erdem Çankaya","Balçova")
+        
+#erdeminh parasını aldı benim parama ekledi
+personYarko.money=personYarko.money +personErdem.money
+print(personYarko.money)
+
+#private yapınca erişmek istediğimizde AttributeError hatası verir değil değiştirmek görüntülemekte yapılamaz
+
+personYarko.__money #private yaptıktan sonra bu şekilde çağırabiliyoruz içerdeki methodu
