6c3bfa01059ae9a2a0444a8922866ea563cceca9
yarko35 <yarkinokular@gmail.com>
2021-03-09 14:42:42 +0300

    abs class is finished

--- Python AI/OOP/abstract_Classes.py
+++ Python AI/OOP/abstract_Classes.py
@@ -1,6 +1,32 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Mar  3 05:39:33 2021
+#soyut klaslar
+#super class ve sub class(child)
+#super class sub class için gereken sınıfları soyut olarka tutar
 
-@author: yarki
-"""
+#abstract base class demektir
+from abc import ABC, abstractmethod
+
+class animal(ABC):#super class
+    @abstractmethod
+    def walk(self):pass
+    @abstractmethod
+    def run(self):pass
+    #abstract sınıfta kullanılan bir method sub classda kullanılmak ZORUNDAdır.
+
+    #artık animal classı abstract bir sınıf olmuş oluyor
+class bird(animal):#sub class
+    def __init__(self):
+        print("bird")
+    def walk(self):
+        print("Kuşlar aynı zamanda yürür")
+    
+    def run(self):
+        print("Kuşlar koşamaz.")
+
+
+#Temel Kurallar
+#Eğer bir abstract sınıf oluşturduysak ve bunun bir child ı varsa abstract sınıf içerisinde yer alan methodlar child da da olmak zorundadır
+# abc import ettikten sonra object kısmına ABC diye geçilmelidir aksi takdirde hala daha abstract sınıf olduğu anlaşılmaz sistem tarafından.
+#sınıf içerisindeki her methodun(fonksiyonun) başına  @abstractmethod diye eklenmelidir.
+#Abstract kullanılma sebebi bir şablon yaratmaktır sanki bir yorum satırı açmış gibi içerisine barınmasını istediğimiz methodları gireriz. 
+#Abstract bir sınıftaki methodda sadece bir tane  @abstractmethod olması yeterlidir ama hangi methodun başında  @abstractmethod varsa o methodu kullanmayı zorunlu kılar.
+#Eğer ki run methodunun başındaki  @abstractmethod ı kaldırırsak run methodunun sub classtaki kullanılma zorunluluğu kalkmış olur
